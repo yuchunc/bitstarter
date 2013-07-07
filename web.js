72d1e0551a037ecde6d0694e93b11c43b9fae6ef
Mickey <Mickey@d@home.com>
2013-07-07 14:15:01 +0000

    try try

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-  result = buffer.toString(fs.readFileSync(index.html));
+  result = buffer.toString(fs.readFileSync("index.html"));
 
   response.send(result);
 });
